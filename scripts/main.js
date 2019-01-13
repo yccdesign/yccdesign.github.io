3807f0b16d9af62914b9b05108ebc60bf19e392b
phoebe7979 <phoebe7979@gmail.com>
2019-01-13 09:00:20 -0500

    update grogo project

--- scripts/main.js
+++ scripts/main.js
@@ -10,6 +10,9 @@ function navIconClick() {
 
 
 
+
+
+
 $(document).ready(function(){
 
 /*skip to content*/
@@ -71,8 +74,38 @@ $(document).ready(function(){
   });
 
 
-
 });
+
+
+/*Slideshow in project page*/
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
 
 /*Accoridan in project page*/
 var acc = document.getElementsByClassName("accordion");
