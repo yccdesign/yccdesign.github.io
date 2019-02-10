e8f9110804597243155b6e3aad958bef4de8de45
phoebe7979 <phoebe7979@gmail.com>
2019-02-10 00:38:58 -0500

    update grogo

---
+++ scripts/modal.js
@@ -0,0 +1,47 @@
+// Get the scenariomodal1
+var modal = document.getElementsByClassName('scenariomodal');
+// Get the button that opens the modal
+var btn = document.getElementsByClassName("scenarioBtn");
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("modalclose");
+// When the user clicks the button, open the modal
+
+btn[0].onclick = function() {
+    modal[0].style.display = "block";
+}
+btn[1].onclick = function() {
+    modal[1].style.display = "block";
+}
+btn[2].onclick = function() {
+    modal[2].style.display = "block";
+}
+btn[3].onclick = function() {
+    modal[3].style.display = "block";
+}
+btn[4].onclick = function() {
+    modal[4].style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span[0].onclick = function() {
+    modal[0].style.display = "none";
+}
+span[1].onclick = function() {
+    modal[1].style.display = "none";
+}
+span[2].onclick = function() {
+    modal[2].style.display = "none";
+}
+span[3].onclick = function() {
+    modal[3].style.display = "none";
+}
+span[4].onclick = function() {
+    modal[4].style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
