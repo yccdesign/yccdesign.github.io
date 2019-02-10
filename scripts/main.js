e4612279df096b3aa41aad3302842c9d0e9bc21f
phoebe7979 <phoebe7979@gmail.com>
2019-02-09 21:09:55 -0500

    update

--- scripts/main.js
+++ scripts/main.js
@@ -9,6 +9,34 @@ function navIconClick() {
 }
 
 
+// Get the scenariomodal1
+var modal = document.getElementsByClassName('scenariomodal');
+// Get the button that opens the modal
+var btn = document.getElementsByClassName("scenarioBtn");
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("modalclose");
+// When the user clicks the button, open the modal
+btn[0].onclick = function() {
+    modal[0].style.display = "block";
+}
+
+btn[1].onclick = function() {
+    modal[1].style.display = "block";
+}
+// When the user clicks on <span> (x), close the modal
+span[0].onclick = function() {
+    modal[0].style.display = "none";
+}
+
+span[1].onclick = function() {
+    modal[1].style.display = "none";
+}
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
 
 
 
@@ -234,7 +262,6 @@ function showSlides(n) {
 
 
 
-
 /*Slideshow for persona*/
 //var slideIndexPersona = 1;
 //showSlidesPersona(slideIndexPersona);
@@ -264,6 +291,7 @@ function showSlides(n) {
 //  slides[slideIndexPersona-1].style.display = "block";
 //  dots[slideIndexPersona-1].className += " active";
 //}
+
 
 
 
